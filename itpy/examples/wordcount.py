21d2e8edbb24cdd283c39ff1bbec0bf1fc03ee57
Jason Liu <jx5liu@uwaterloo.ca>
2014-12-01 09:54:16 -0500

    cleaned up word count

--- itpy/examples/wordcount.py
+++ itpy/examples/wordcount.py
@@ -23,12 +23,12 @@ from itpy import Itpy
 from itpy.lambdas import str_split, str_strip, identity, one, add, valuef, pair_to_str
 
 
-fn = str(join(dirname(__file__), 'zen.txt'))
+file_name = str(join(dirname(__file__), 'zen.txt'))
 
-tokenized = Itpy.from_file(fn)\
-    .flatmap(str_split(" "))\
-    .map(str_strip)\
-    .reduceby(reducer=add, key=identity, value=one)\
-    .sort(key=valuef, reverse=True)\
+tokenized = Itpy.from_file(file_name) \
+    .flatmap(str_split(" ")) \
+    .map(str_strip) \
+    .reduceby(reducer=add, key=identity, value=one) \
+    .sort(key=valuef, reverse=True)
 
 tokenized.map(pair_to_str).to_stdout()
