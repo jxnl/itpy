2d013d1d85b963f5671152036ab727680029786b
jxnl <jason@jxnl.co>
2016-06-21 20:34:56 -0400

    Just to create the branch.

---
+++ build/lib/itpy/__init__.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+
+"""
+itpy
+~~~~
+
+Built on the back of itertools, `itpy` allows easy list processing through chaining methods.
+Everything is a lazy evaluated generating function so nothing happens until you call a method
+with side effects. This allows for a fast memory effecient 'keep what you need' way of processing
+large ammounts of data!
+
+"""
+from __future__ import print_function
+
+from itpy.decorators import iter_wraps, term_wraps
+from itpy.helpers import identity, getitem
+import itpy.transforms as transforms
+import itpy.summary as summary
+import itpy.iio as iio
+
+
+class Itpy(object):
+    VALUE = None
+
+    def __init__(self, iterable=None, itpy=None):
+        if iterable:
+            self._iter = iter(iterable)
+            self._src = type(iterable)
+        if itpy:
+            self._stack = itpy._stack
+            self._src = itpy._src
+        else:
+            self._stack = []
+
+    """
+    Transformations
+    ~~~~~~~~~~~~~~~
+
+    These methods provide you with the power to transforms your Itpy datastreams into other data streams through
+    chaining methods together.
+    """
+
+    @iter_wraps(transforms.map)
+    def map(self, function):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.flatmap)
+    def flatmap(self, function_to_list):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.filter)
+    def filter(self, predicate):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.filterfalse)
+    def filterfalse(self, predicate):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.sort)
+    def sort(self, cmp=None, key=None, reverse=False):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.reduceby)
+    def reduceby(self, reducer, key=getitem(0), value=getitem(1)):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.groupby)
+    def groupby(self, key=identity, value=identity):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.union)
+    def union(self, *iterable):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.top)
+    def top(self, max_size=1, key=None):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.takewhile)
+    def takewhile(self, predicate):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.dropwhile)
+    def dropwhile(self, predicate):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.take)
+    def take(self, max):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.slice)
+    def slice(self, *args):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.distinct)
+    def distinct(self):
+        return Itpy(itpy=self)
+
+    @iter_wraps(transforms.intercept)
+    def intercept(self, function):
+        return Itpy(itpy=self)
+
+    @term_wraps(summary.frequency)
+    def frequency(self):
+        return Itpy.VALUE
+
+    @term_wraps(summary.mean)
+    def mean(self):
+        return Itpy.VALUE
+
+    @term_wraps(summary.online_variance)
+    def variance(self):
+        return Itpy.VALUE
+
+    @term_wraps(summary.for_each)
+    def for_each(self, function):
+        return Itpy.VALUE
+
+    @term_wraps(summary.reduce)
+    def reduce(self, reducer):
+        return Itpy.VALUE
+
+    @term_wraps(summary.size)
+    def size(self):
+        return Itpy.VALUE
+
+    @staticmethod
+    @iter_wraps(iio.from_file)
+    def from_file(path_to_file, buffer=1):
+        return Itpy()
+
+    @staticmethod
+    @iter_wraps(iio.from_stdin)
+    def from_stdin(self):
+        return Itpy()
+
+    @term_wraps(iio.to_stdout)
+    def to_stdout(self):
+        return Itpy.VALUE
+
+    @term_wraps(iio.to_file)
+    def to_file(self, path_to_file):
+        return Itpy.VALUE
+
+    """
+    Sketching methods
+    ~~~~~~~~~~~~~~~~~
+
+    These algorithms have limited memory available to them (much less than the input size) and also limited
+    processing time per item. These constraints may mean that an algorithm produces an approximate answer
+    based on a summary or "sketch" of the data stream in memory.
+
+    TODO : HyperLogLog, Count-min
+
+    """
+
+    # @term_wraps(sketch.count_distinct_approx)
+    # def count_distinct_approx(self, init_cap=200, err_rate=0.001):
+    # return Itpy.VALUE
+
+    # @term_wraps(sketch.to_bloomfilter)
+    # def to_bloomfilter(self, init_cap=200, err_rate=0.001):
+    # return Itpy.VALUE
+
+
+    @property
+    def _(self):  # Collect the elements of the iter into a list
+        return list(self)
+
+    def __repr__(self):
+        return "Itpy({}).{}".format(self._src, ".".join(self._stack))
+
+    def __radd__(self, other):
+        if other.__iter__:
+            return self.union(iter(other))
+
+    def __iter__(self):
+        for item in self._iter:
+            yield item
+
+    def next(self):
+        return self._iter.next()
---
+++ build/lib/itpy/transforms.py
@@ -0,0 +1,466 @@
+# -*- coding: utf-8 -*-
+
+"""
+itpy.transforms
+~~~~~~~~~~~~~~~
+
+This module contains the all the combinators and other methods allowed on
+iterables. All functions must consume iterables as it's first argument
+and return and an iterable.
+
+"""
+
+from collections import defaultdict
+from itpy.helpers import identity, getitem
+
+import itertools as it
+import types
+
+
+def distinct(iterable):
+    """
+    Make an iterator with only the distinct elements of the previous.
+
+    :param iterable:
+    """
+
+    def distincting(iterable_):
+        set_of_distinct_values = set()
+        for i in iterable_:
+            if i not in set_of_distinct_values:
+                set_of_distinct_values.add(i)
+                yield i
+
+    return distincting(iterable)
+
+
+def intercept(iterable, function):
+    """
+    Make an iterable from the original one but intercept the value and
+    evaluate the function for the value.
+    This may be valuable for logging and debuging.
+
+    :param iterable:
+    :param function:
+    """
+
+    def intercepting(iterable_):
+        for item in iterable_:
+            function(item)
+            yield item
+
+    return intercepting(iterable)
+
+
+def drop(iterable, n):
+    """
+    Make and iterator of the original iterator without the first n elements
+
+    :param iterable:
+    :param n:
+    :return:
+    """
+    counter = 0
+    for element in iterable:
+        if counter < n:
+            counter += 1
+        else:
+            yield element
+
+
+def dropwhile(iterable, predicate):
+    """
+    Make an iterator that drops elements from the iterable as long as the
+    predicate is true
+
+    Note, the iterator does not produce any output until the predicate
+    first becomes false, so it may have a lengthy start-up time.
+
+    :param iterable:
+    :param predicate:
+    """
+    return iter(it.dropwhile(predicate, iterable))
+
+
+def filter(iterable, predicate):
+    """
+    Make an iterator that filters elements from iterable returning only those
+    for which the predicate is True
+    If predicate is None, return the items that are true.
+
+    :param iterable:
+    :param predicate:
+    """
+
+    for x in iterable:
+        if predicate(x):
+            yield x
+
+
+def filterfalse(iterable, predicate):
+    """
+    Make an iterator that filters elements from iterable returning only those
+    for which the predicate is False.
+    If predicate is None, return the items that are false
+
+    :param iterable:
+    :param predicate:
+    """
+    for x in iterable:
+        if not predicate(x):
+            yield x
+
+
+def find(iterable, predicate, n=1):
+    """
+    Produce the first element in the iterable that satisfies the predicate
+
+    :param iterable:
+    :param predicate:
+    """
+    counter = 0
+    for element in iterable:
+        if predicate(element) and counter < n:
+            yield element
+
+
+def flatmap(iterable, function_to_list):
+    """
+    Make an iterator that returns the elements of the lists produced
+    by mapping function_to_list onto the original iterable.
+
+    :param iterable:
+    :param function_to_list:
+    """
+    for element in iterable:
+        list_block = function_to_list(element)
+        for result_value in list_block:
+            yield result_value
+
+
+def flatten(iterable):
+    """
+    Make an iterator from a iterable of iterables
+
+    :param iterable:
+    """
+    for element_iterable in iterable:
+        for element in element_iterable:
+            yield element
+
+
+def fold(iterable, func, base):
+    """
+    Folds the elements of this iterable using an associative binary operator
+
+    :param iterable:
+    :param func:
+    """
+    acc = base
+    for element in iterable:
+        acc = func(acc, element)
+    return acc
+
+
+def forall(iterable, predicate):
+    """
+    Test if predicate holds for all elements in iterator
+
+    :param iterable:
+    :param predicate:
+    :return:
+    """
+    for element in iterable:
+        if not predicate(element):
+            return False
+    return True
+
+
+def groupby(iterable, key=identity, value=identity):
+    """
+    Make a dict that returns consecutive keys and groups from the iterable
+    The key is computed each element by keyfunc.
+
+    :param iterable:
+    :param key:
+    """
+    group_by_collection = defaultdict(list)
+    for element in iterable:
+        k = key(element)
+        if isinstance(value, types.FunctionType):
+            group_by_collection[k].append(value(element))
+        else:
+            group_by_collection[k].append(value)
+
+    return group_by_collection
+
+
+def grouped(iterable, n):
+    """
+    Make an iterator of elements partitioned by the n
+    :param iterable:
+    :param n:
+    """
+    batch_window = [None for _ in range(n)]
+    cur_size = 0
+    for item in iterable:
+        batch_window[cur_size] = item
+        cur_size += 1
+        if cur_size >= n:
+            batched = batch_window[:]
+            batch_window = [None for _ in range(n)]
+            cur_size = 0
+            yield batched
+
+
+def has_definite_size(iterable):
+    """
+    Tests whether an iterable is know to have a finite size
+
+    :param iterable:
+    """
+    return hasattr(iterable, '__len__')
+
+
+def map(iterable, function):
+    """
+    Make an iterator that computes the function using arguments
+    from the original iterable.
+
+    :param iterable:
+    :param function:
+    """
+    for x in iterable:
+        yield function(x)
+
+
+def max(iterable):
+    """
+    :param iterable:
+    :return max element:
+    """
+    max = None
+    for element in iterable:
+        if max and element > max:
+            max = element
+    return max
+
+
+def maxBy(iterable, function):
+    """
+
+    :param iterable:
+    :param function:
+    """
+    return max(map(iterable, function))
+
+
+def min(iterable):
+    """
+
+    :param iterable:
+    :return:
+    """
+    min = None
+    for element in iterable:
+        if min and element < min:
+            min = element
+    return min
+
+
+def minBy(iterable, function):
+    """
+
+    :param iterable:
+    :param function:
+    """
+    return min(map(iterable, function))
+
+
+def partition(iterable, predicate):
+    """
+    Partitions this an iterable collection
+    in two traversable collections according to a predicate.
+
+    :param iterable:
+    :param predicate:
+    """
+    passes = list()
+    fails = list()
+    for element in iterable:
+        if predicate(element):
+            passes.append(element)
+        else:
+            fails.append(element)
+    return passes, fails
+
+
+# TODO
+def permutations(iterable):
+    """
+    Makes iterable from permutations of iterable
+
+    :param iterable:
+    """
+    pass
+
+
+# TODO
+def intersect(iterable1, iterable2):
+    """
+    Returns iterable of the multiset intersection of two iterables
+
+    :param iterable1:
+    :param iterable2:
+    """
+    pass
+
+
+def slice(iterable, *args):
+    """
+    Make an iterator that returns selected elements from the iterable.
+    If start is non-zero, then elements from the iterable are skipped
+    until start is reached. Afterward, elements are returned consecutively
+    unless step is set higher than one which results in items being skipped.
+
+    If stop is None, then iteration continues until the iterator is exhausted;
+    otherwise, it stops at the specified position. Unlike regular slicing,
+    slice() does not support negative values for start, stop, or step.
+
+    :param iterable:
+    :param args:
+    """
+    return iter(it.islice(iterable, *args))
+
+
+def size(iterable):
+    """
+    Return the size of the iterable, None if has no attr `__len__`
+
+    :param iterable:
+    """
+    try:
+        return iterable.__len__()
+    except AttributeError:
+        return None
+
+
+def sort(iterable, cmp=None, key=None, reverse=False):
+    """
+    Make and iterator that is sorted on a specific key, if key is None, sort on
+    natural ordering.
+
+    :param iterable:
+    :param cmp:
+    :param key:
+    :param reverse:
+
+    TODO: Make lazy
+
+    """
+    sorted_iterable = sorted(iterable, cmp=cmp, key=key, reverse=reverse)
+
+    return iter(sorted_iterable)
+
+
+def top(iterable, max_size=1, key=None):
+    """
+    Make an iterable of the top max_size elements of the original iterable
+    sorted on keyfunc, if keyfunc is None sort on the natural ordering.
+
+    :param iterable:
+    :param max_size:
+    :param key:
+    """
+
+    # An import is done here to avoid polluting the namespace
+    from heapq import heapreplace, heapify
+
+    top_k_values = []
+
+    for idx, item in enumerate(iterable):
+        if idx < max_size:
+            top_k_values.append(item)
+        elif idx == max_size:
+            heapify(top_k_values)
+        elif idx > max_size:
+            heapreplace(top_k_values, item)
+
+    return iter(sorted(top_k_values, key=key, reverse=True))
+
+
+def take(iterable, n):
+    """
+    Make an iterator that contains the first n elements from the original
+
+    :param iterable:
+    :param n:
+    """
+
+    def taking(iterable_):
+        for i, e in enumerate(iterable_):
+            if i < n:
+                yield e
+
+    return taking(iterable)
+
+
+def takewhile(iterable, predicate):
+    """
+    Make an iterator that returns elements from the iterable as long as the
+    predicate is true.
+
+    :param iterable:
+    :param predicate:
+    """
+    return iter(it.takewhile(predicate, iterable))
+
+def reduceby(iterable, reducer, key=getitem(0), value=getitem(1)):
+    """
+    Make an iterator that returns the merged values for each key using an
+    associative reduce function.The values contained in this iterable must be
+    2-tuples in the form (k, v).
+    :param iterable:
+    :param reducer:
+    :param key:
+    :param value:
+    """
+
+    def reducing(iterable_, reducer_, key_, value_):
+        group_by = dict()
+
+        for element in iterable_:
+            (k, v) = (key_(element), value_(element))
+
+            if k in group_by:
+                group_by[k] = reducer_(group_by[k], v)
+            else:
+                group_by[k] = v
+
+        for ke in group_by:
+            yield (ke, group_by[ke])
+
+    return reducing(iterable, reducer, key, value)
+
+
+
+def toArray(iterable):
+    import array
+
+    return array.array(iterable)
+
+
+def toList(iterable):
+    return list(iterable)
+
+
+def union(iterable, *iterables):
+    """
+    Make an iterator that returns elements from the first iterable until it
+    is exhausted, then proceeds to the next iterable, until all of the iterables
+    are exhausted. Used for treating consecutive sequences as a single sequence.
+
+    :param iterable:
+    :param iterables:
+    """
+    return iter(it.chain(iterable, *iterables))
